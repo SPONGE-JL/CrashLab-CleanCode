ddbea86c4066654e27d33cde032757d8a5936c74
sponge <dev2sponge@gmail.com>
2021-07-15 19:23:05 +0900

    예제 - 의미있는  맥락으로 개선

--- Chapter02-MeaningfulNames/sample-meaning-ful-context.js
+++ Chapter02-MeaningfulNames/sample-meaning-ful-context.js
@@ -1,5 +1,47 @@
-const join = (str, str2) => {
-  dbRepository.add({ str, str2 });
+import UserRepository from "./dummy-user-repository";
+import EncryptTool from "./dummy-encrypt-tool";
+
+const signUpNewUser = (submittedUserInfo) => {
+  // Default Validation
+  const { userId, userPw } = submittedUserInfo;
+  if (!userId || !userPw) {
+    console.error(
+      `SIGN-UP > ERROR > Invalid UserInfo: ${JSON.stringify(submittedUserInfo)}`
+    );
+    return null;
+  }
+
+  // ID Validation
+  const isUniqueUserId = UserRepository.findById(userId) == null ? true : false;
+  if (!isUniqueUserId) {
+    console.error(`SIGN-UP > ERROR > Duplicated ID: ${userId}`);
+    return null;
+  }
+
+  // Password Encryption
+  const encryptedPw = EncryptTool.encrypt(userPw);
+  const encryptedUser = { userId, userPw: encryptedPw };
+
+  // Insert New User
+  const signedNewUser = UserRepository.save(encryptedUser);
+  console.log(`SIGN-UP > INFO > Welcome New User: ${signedNewUser.userId}`);
+  return signedNewUser;
 };
 
-join("dev-chloe", "#2102103#"); // 이 코드가 회원가입 로직이라는 것을 눈치챌 수 있는가?
+// ---
+
+// If Submitted..
+const paramUserId = "dev-chloe";
+const paramUserPw = "#2102103#";
+
+// Signup with submitted User Information
+const submittedUserInfo = {
+  userId: paramUserId,
+  userPw: paramUserPw,
+};
+const signedNewUser = signUpNewUser(submittedUserInfo);
+
+/*
+ * 만약 이 지점(---)을 기준으로 파일이 나누어진다 하더라도,
+ * 각각의 함수, 변수의 이름에 의도를 명확히 드러내어 맥락을 전달한다.
+ */
